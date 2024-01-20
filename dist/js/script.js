4230dc21433e037f6f54c6404b143eb9d7bac403
Gilang Fauzi <gilangfauzi023@gmail.com>
2024-01-21 02:41:05 +0700

    update protfolio tailwindcss

---
+++ dist/js/script.js
@@ -0,0 +1,14 @@
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if(window.pageYOffset > fixedNav) header.classList.add('navbar-fixed');
+    else header.classList.remove('navber-fixed');
+}
+
+const hamburger =document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
